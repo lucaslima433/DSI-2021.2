3b85e220199f8f5a7d791629cbad9b28af264213
lucaslima433 <lucaslima433@hotmail.com>
2022-09-25 14:17:28 -0300

    add: Finalizando Topico 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: Homepage(),
+      theme: ThemeData(          // Add the 5 lines from here... 
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: const Homepage(),
     );
   }
 }
@@ -26,6 +32,7 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   String _view = "list";
 
@@ -34,6 +41,13 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Startup Name Generator'),
+          actions: [
+          IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions',
+            ),
+          ],
         ),
           
         body: Center(
@@ -49,9 +63,38 @@ class _HomepageState extends State<Homepage> {
             }
             setState(() {});
           },
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.deepPurple,
           child: const Icon(Icons.swipe),
         ),
+    );
+  }
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+
+
+          final divided = tiles.isNotEmpty ? ListTile.divideTiles(context: context,tiles: tiles,).toList() : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 
@@ -74,12 +117,32 @@ class _HomepageState extends State<Homepage> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
+
+        final alreadySaved = _saved.contains(_suggestions[index]);
         
         return ListTile(
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
           ),
+          
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+          ),
+
+          onTap: () {          // NEW from here ...
+                setState(() {
+                  if (alreadySaved) {
+                    _saved.remove(_suggestions[index]);
+                  } else {
+                    _saved.add(_suggestions[index]);
+                  }
+                }
+              );    
+            },
+
         );
       },
     );
@@ -91,19 +154,48 @@ class _HomepageState extends State<Homepage> {
           const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
       padding: const EdgeInsets.all(16.0),
 
-      itemBuilder: /*1*/ (context, i) { /*3*/
-        if (i >= _suggestions.length) {
+      itemBuilder: /*1*/ (context, index) { /*3*/
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
 
+        final alreadySaved = _saved.contains(_suggestions[index]);
 
         return Card(
           borderOnForeground: true,
-          child: Center(
-            child: Text(
-              _suggestions[i].asPascalCase,
-              style: _biggerFont,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                Text(
+                _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              IconButton(
+                icon: Icon(
+                alreadySaved ? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved ? Colors.red : null,
+                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+              ),
+              
+              onPressed: () => {
+                setState(() {
+                    if (alreadySaved) {
+                      _saved.remove(_suggestions[index]);
+                    } else {
+                      _saved.add(_suggestions[index]);
+                    }
+                  }
+                )
+              },
+
+              )  
+            ],    
+
           )
         );
       },
